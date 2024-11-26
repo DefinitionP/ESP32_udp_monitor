0f57cd8d5cd6c4782f2618f96a8a64960354ed0c
Nikita <belyakov.nickita444@yandex.ru>
2024-11-26 11:15:26 +0400

    Добавьте файлы проекта.

---
+++ udp_test/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+
+using System.Threading;
+using System.Net.NetworkInformation;
+
+
+
+namespace udp_test
+{
+    internal class Program
+    {
+        public static int PORT = 8888;
+        public static IPAddress desktop_ip = null;
+        public static IPAddress device_ip = null;
+        public static int device_port = 0;
+        private static Socket socket;
+        private static IPEndPoint ep;
+        public static bool clear_flag = false;
+        static void Main(string[] args)
+        {
+            RemoteNode.Nodes.Add(new RemoteNode(8888, "1", ConsoleColor.DarkGreen));
+            RemoteNode.Nodes.Add(new RemoteNode(8889, "2", ConsoleColor.DarkBlue));
+            RemoteNode.Nodes.Add(new RemoteNode(8890, "3", ConsoleColor.DarkMagenta));
+            RemoteNode.Nodes.Add(new RemoteNode(8891, "4", ConsoleColor.DarkCyan));
+            RemoteNode.Nodes.Add(new RemoteNode(8892, "5", ConsoleColor.DarkYellow));
+
+            Console.WindowWidth = Console.LargestWindowWidth - 30;
+            print_my_ip();
+            Console.WriteLine();
+            RemoteNode.StartListening();
+
+            Thread thread = new Thread(key_parse);
+            thread.IsBackground = true;
+            thread.Start();
+
+            while (true)
+            {
+                if (clear_flag == true)
+                {
+                    clear_flag = false;
+                    Console.Clear();
+                }
+                foreach (var node in RemoteNode.Nodes)
+                {
+                    if (node.messages.Count() > 0)
+                    {
+                        Console.ForegroundColor = node.consoleColor;
+                        Console.WriteLine(node.messages.First());
+                        Console.ForegroundColor = ConsoleColor.White;
+                        node.messages.RemoveAt(0);
+                    }
+                }
+            }
+        }
+        static void key_parse()
+        {
+            ConsoleKeyInfo key = Console.ReadKey(true);
+            while (true)
+            {
+
+                if (key.Key == ConsoleKey.C)
+                {
+                    clear_flag = true;
+                }
+                key = Console.ReadKey(true);
+            }
+        }
+        static void print_my_ip()
+        {
+
+            IPAddress[] ipaddress = Dns.GetHostAddresses(Dns.GetHostName());
+            desktop_ip = ipaddress.Last();
+            Console.Write($"IP устройства: {desktop_ip}");
+        }
+    }
+}
---
+++ udp_test/RemoteNode.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Net;
+
+
+namespace udp_test
+{
+    internal class RemoteNode
+    {
+        public static List<RemoteNode> Nodes = new List<RemoteNode>();
+        public static IPAddress desktop_ip = IPAddress.None;
+
+        public ConsoleColor consoleColor;
+        public int port;
+        public UdpClient udp;
+        public IPEndPoint endPoint;
+        public string name;
+        public Thread listenThread;
+        public List<string> messages = new List<string>();
+        public RemoteNode(int port, string name, ConsoleColor consoleColor)
+        {
+            if (desktop_ip == IPAddress.None)
+            {
+                IPAddress[] ipaddress = Dns.GetHostAddresses(Dns.GetHostName());
+                desktop_ip = ipaddress.Last();
+            }
+            this.consoleColor = consoleColor;
+            this.port = port;
+            this.name = name;
+            udp = new UdpClient(port);
+            endPoint = new IPEndPoint(desktop_ip, port);
+            listenThread = new Thread(Listen);
+            listenThread.Priority = ThreadPriority.Highest;
+        }
+
+        public static void StartListening()
+        {
+            foreach (var node in Nodes)
+            {
+                node.listenThread.Start();
+            }
+        }
+        public bool Available()
+        {
+            return udp.Available > 0;
+        }
+        public string ReadMessage()
+        {
+            byte[] data = udp.Receive(ref endPoint);
+            DateTime dateTime = DateTime.Now;
+            StringBuilder sb = new StringBuilder();
+            sb.Append(dateTime.ToString());
+            sb.Append($" recieved from \"{name}\": ");
+            sb.Append($"\"{Encoding.ASCII.GetString(data, 0, data.Length)}\"");
+            return sb.ToString();
+        }
+        public void Listen()
+        {
+            while (true)
+            {
+                if (Available()) messages.Add(ReadMessage());
+            }
+        }
+    }
+}
